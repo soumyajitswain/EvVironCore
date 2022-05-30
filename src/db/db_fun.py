f9c0f78ea89cc0a61810bf420ac0615b7bdf54ed
soumyajit <soumyajit.swain@gmail.com>
2022-05-30 20:57:10 +0530

    TransactionManager db func is added

--- src/db/db_fun.py
+++ src/db/db_fun.py
@@ -1,5 +1,7 @@
+import json
+from time import time
 import sqlalchemy
-from db.sqlalchemy_db_check import Chargebox, Users as user
+from db.sqlalchemy_db_check import Chargebox, Connector, ConnectorStatus, TransactionStart, TransactionStop, TransactionStopFail, Users as user
 from sqlalchemy.orm import sessionmaker, Session
 
 engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:root@127.0.0.1:3306/environ")
@@ -16,11 +18,52 @@ class UserDbFunc:
         return user
 
 class ChargeBoxFunc:
-    def  _get_all_charge_box(self):
+    def get_all_charge_box(self):
         session = Session()
         query = session.query(Chargebox).filter(ChargeBoxFunc.fw_update_status == 'N')
-        _result = query.all
+        res = query.all
+        _result = json.dumps([dict(r) for r in res])
         return _result
 
+    def get_charge_connector_detail_by_id(self, charge_box_id):
+        session = Session()
+        join_query = session.query(Chargebox, Connector, ConnectorStatus )\
+            .join(Connector, Connector.charge_box_id == Chargebox.charge_box_id)\
+            .join(ConnectorStatus, ConnectorStatus.connector_pk == Connector.connector_pk)\
+            .filter(Chargebox.charge_box_id == charge_box_id)
         
-        
+        result = json.dumps([dict(r) for r in join_query])
+
+        return result
+
+class TransactionManager:
+    def start_transaction(self, connector_id, id_tag):
+        session = Session()
+        ts = TransactionStart(connector_pk = connector_id, id_tag = id_tag, start_timestamp = time.time(), event_timestamp = time.time(), start_value = '200')
+        session.add(ts)
+        session.commit()
+
+    def stop_transaction(self, transaction_pk, stop_value, stop_reason):
+        session = Session()
+        ts = TransactionStop(transaction_pk = transaction_pk, \
+            stop_value = stop_value, stop_reason = stop_reason, event_timestamp = time.time(), event_actor = 1)
+        session.add(ts)
+        session.commit()
+
+    def get_transaction(self, transaction_id):
+        session = Session()
+        query = session.query(TransactionStart, TransactionStop, TransactionStopFail)\
+            .join(TransactionStop, TransactionStart.transaction_pk == TransactionStop.transaction_pk)\
+            .join(TransactionStopFail, TransactionStopFail.transaction_pk == TransactionStart.transaction_pk)
+        res = query.all
+        _result = json.dumps([dict(r) for r in res])
+
+    def stop_fail_transaction(self, transaction_pk, stop_value, stop_reason, fail_reason):
+        session = Session()
+        ts = TransactionStop(transaction_pk = transaction_pk, \
+          stop_value = stop_value, stop_reason = stop_reason, event_timestamp = time.time(), \
+              event_actor = 1, fail_reason = fail_reason)
+        session.add(ts)
+        session.commit()
+
+                    
